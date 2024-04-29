49c46c825442d1d732c0139e80ed6a1ed7246726
George Karountzos <george3-2001@hotmail.com>
2024-04-29 23:39:08 +0300

    restarted project

---
+++ frontpage.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="images/logo-tg-trans.svg">
+    <title>Tech Gear - Gear up for the Future!</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&family=Roboto+Mono:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style2.css">
+</head>
+
+<body>
+    <section id="front">
+        <div class="grid-item logo">
+            <img src="images\logo-techgear-trans.svg">
+        </div>
+        <div class="front-text">
+            <h1>Μερικά απο τα προϊόντα του καταστήματός μας</h1>
+        </div>
+        <div class="item1 front-products">
+            <button>
+                <div class="card item2" style="width: 15rem;">
+                    <img src="images\iphone15promax.jpg" class="card-img-top" alt="xiaomi_buds">
+                    <div class="card-body">
+                        <p class="card-text">iPhone 15 Pro Max</p>
+                    </div>
+                </div>
+            </button>
+        </div>
+        <div class="item2 front-products">
+            <button>
+                <div class="card item1" style="width: 15rem;">
+                    <img src="images\xiaomibuds4.jpg" class="card-img-top" alt="xiaomi_buds">
+                    <div class="card-body">
+                        <p class="card-text">Redmi Buds 4 Active</p>
+                    </div>
+                </div>
+            </button>
+        </div>
+        <div class="item3 front-products">
+            <button>
+                <div class="card item3" style="width: 15rem;">
+                    <img src="images\sgalaxys24.jpg" class="card-img-top" alt="xiaomi_buds">
+                    <div class="card-body">
+                        <p class="card-text">Samsung Galaxy S24</p>
+                    </div>
+                </div>
+            </button>
+        </div>
+        </div>
+        <div class="item4 redirect">
+            <button type="button" class="btn btn-success"><a href="login.php">Σύνδεση</a></button>
+        </div>
+        <div class="item5">
+            <p>Δεν έχεις λογαριασμό; Γίνε τώρα μέλος!</p>
+        </div>
+        <div class="item6 redirect">
+            <button type="button" class="btn btn-primary"><a href="register.php">Εγγραφή</a></button>
+        </div>
+
+
+
+    </section>
+
+    <script src="script.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+</body>
+
+</html>
